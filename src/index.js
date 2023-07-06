2715fc5feaef5a5c84aa37070605c0f8599cf8d9
Philip Snyder <psnyder385@gmail.com>
2023-07-06 14:13:31 -0400

    boilerplate

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import Header from "./Components/Header";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <BrowserRouter>
+        <Header />
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
